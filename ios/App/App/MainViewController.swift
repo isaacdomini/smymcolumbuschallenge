13fc6cfecb5cea0b57e4e17e1e8207344d4e9a98
Isaac Domini <me@isaacdomini.com>
2025-11-13 20:40:27 -0500

    update

--- ios/App/App/MainViewController.swift
+++ ios/App/App/MainViewController.swift
@@ -3,11 +3,13 @@ import Capacitor
 
 class MainViewController: CAPBridgeViewController {
 
+  // Use viewDidLoad, which is called *after* the view hierarchy has been loaded.
+  // This is the correct place to modify properties of the view.
   override func viewDidLoad() {
     super.viewDidLoad()
     
     // Enable the iOS swipe back gesture
-    self.webView.allowsBackForwardNavigationGestures = true;
+    self.webView?.allowsBackForwardNavigationGestures = true
   }
 
 }
